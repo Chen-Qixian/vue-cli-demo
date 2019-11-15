6bb0dd082d7c3fa2c01955870538ac400ed7756a
chenqixian <chenqixian@buaa.edu.cn>
2019-11-15 15:03:00 +0800

    初始化布局，配置导航栏

---
+++ project/src/components/Right.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div v-if="kk==1">
+            111111
+        </div>
+        <div v-if="kk==2">
+            222222
+        </div>
+        <div v-if="kk==3">
+            333333
+        </div>
+        <div v-if="kk==4">
+            444444
+        </div>
+    </div>
+</template>
+
+<script>
+    import Msg from './msg.js'
+    export default {
+        data() {
+            return {
+                kk: 0
+            }
+        },
+        mounted: function() {
+            let _this = this;
+            Msg.$on("val", function(m) {
+                _this.kk = m;
+            })
+        }
+    }
+</script>
